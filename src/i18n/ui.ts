29927837dafbe1a1852043153058656b6311edb0
FMColdays <160916989+ErickGonzalezPerez@users.noreply.github.com>
2026-05-10 18:07:28 -0600

    feat: add i18n to proyect

---
+++ src/i18n/ui.ts
@@ -0,0 +1,28 @@
+export const languages = {
+  es: 'Español',
+  en: 'English',
+}
+export const defaultLang = 'es'
+
+export const ui = {
+  es: {
+    'nav.experience':      'Experiencia',
+    'nav.experience.href': '#experiencia',
+    'nav.projects':        'Proyectos',
+    'nav.projects.href':   '#proyectos',
+    'nav.skills':          'Habilidades',
+    'nav.skills.href':     '#habilidades',
+    'nav.about':           'Sobre mí',
+    'nav.about.href':      '#sobre-mi',
+  },
+  en: {
+    'nav.experience':      'Experience',
+    'nav.experience.href': '#experience',
+    'nav.projects':        'Projects',
+    'nav.projects.href':   '#projects',
+    'nav.skills':          'Skills',
+    'nav.skills.href':     '#skills',
+    'nav.about':           'About me',
+    'nav.about.href':      '#about',
+  },
+} as const
