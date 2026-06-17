ddcf5faf00e5a653d6e89da90c274b0e9b9f19e6
FMColdays <160916989+ErickGonzalezPerez@users.noreply.github.com>
2026-06-17 00:31:07 -0600

    feat(skills): remove Angular and Spring Boot, make badges link to docs

--- src/data/tech.ts
+++ src/data/tech.ts
@@ -1,4 +1,3 @@
-import AngularIcon from '@icons/AngularIcon.svg'
 import AstroIcon from '@icons/AstroIcon.svg'
 import DockerIcon from '@icons/DockerIcon.svg'
 import GitIcon from '@icons/GitIcon.svg'
@@ -13,7 +12,6 @@ import PreactIcon from '@icons/PreactIcon.svg'
 import PrismaIcon from '@icons/PrismaIcon.svg'
 import ReactIcon from '@icons/ReactIcon.svg'
 import ShopifyIcon from '@icons/ShopifyIcon.svg'
-import SpringBootIcon from '@icons/SpringBootIcon.svg'
 import SqliteIcon from '@icons/SqliteIcon.svg'
 import TailwindIcon from '@icons/TailwindIcon.svg'
 import TanstackIcon from '@icons/TanstackIcon.svg'
@@ -35,8 +33,8 @@ export interface Tech {
 }
 
 export const TECH_KEYS = [
-  'TypeScript', 'React', 'React Native', 'Preact', 'Angular', 'Astro', 'Tailwind CSS', 'Nativewind',
-  'Node.js', 'Java', 'Spring Boot', 'PostgreSQL', 'MySQL',
+  'TypeScript', 'React', 'React Native', 'Preact', 'Astro', 'Tailwind CSS', 'Nativewind',
+  'Node.js', 'Java', 'PostgreSQL', 'MySQL',
   'Docker', 'Git', 'Shopify', 'WordPress','Sqlite', 'Prisma', '.Net','JavaScript', 'Tanstack', 'Sql Server','Css', 'Resend', 'Clerk',
 ] as const
 
@@ -47,14 +45,12 @@ export const techs: Record<TechKey, Tech> = {
   'React':        { name: 'React',        color: '#61DAFB', href: 'https://react.dev',                                           Icon: ReactIcon       },
   'React Native': { name: 'React Native', color: '#61DAFB', href: 'https://reactnative.dev',                                    Icon: ReactIcon       },
   'Preact':       { name: 'Preact',       color:'#673AB8',  href:'https://preactjs.com',                                         Icon:PreactIcon       },
-  'Angular':      { name: 'Angular',      color: '#DD0031', href: 'https://angular.dev/overview',                                Icon: AngularIcon     },
   'Astro':        { name: 'Astro',        color: '#FF5D01', href: 'https://docs.astro.build',                                    Icon: AstroIcon       },
   'Css':          { name: 'Css',          color: '#663399', href: 'https://developer.mozilla.org/es/docs/Web/CSS',              Icon: TailwindIcon    },
   'Tailwind CSS': { name: 'Tailwind CSS', color: '#06B6D4', href: 'https://tailwindcss.com/docs',                                Icon: TailwindIcon    },
   'Nativewind':   { name: 'Nativewind',   color: '#38BDF8', href: 'https://www.nativewind.dev',                                  Icon: NativewindIcon  },
   'Node.js':      { name: 'Node.js',      color: '#339933', href: 'https://nodejs.org/docs',                                     Icon: NodeIcon        },
   'Java':         { name: 'Java',         color: '#ED8B00', href: 'https://docs.oracle.com/en/java/',                            Icon: JavaIcon        },
-  'Spring Boot':  { name: 'Spring Boot',  color: '#6DB33F', href: 'https://docs.spring.io/spring-boot/',                         Icon: SpringBootIcon  },
   'PostgreSQL':   { name: 'PostgreSQL',   color: '#4169E1', href: 'https://www.postgresql.org/docs/',                            Icon: PostgreSQLIcon  },
   'Sqlite':       { name:'Sqlite',        color:'#006ea1',  href: 'https://sqlite.org',                                          Icon: SqliteIcon      },
   'MySQL':        { name: 'MySQL',        color: '#4479A1', href: 'https://dev.mysql.com/doc/',                                  Icon: MySQLIcon       },
